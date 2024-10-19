760ec2605e1e78f03a8662b9dac0186853354057
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-10-19 17:28:41 +0300

    queries to gain knowledge about the tables.

---
+++ Info about tables.sql
@@ -0,0 +1,8 @@
+-- apply this queries for other tables, here i used employees as a example.
+
+SELECT * FROM employees;
+-- this query retrieve all columns from a Employees table.
+DESCRIBE employees;
+-- this query describe the employees table structure
+SELECT COUNT(*) FROM employees;
+-- this query count tuples in employees table
