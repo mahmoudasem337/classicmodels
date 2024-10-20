dace047d32e7f5f274243c91f3baa26b2d2121b9
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-10-20 19:57:35 +0300

    added some queries and solutions

--- simple queries.sql
+++ simple queries.sql
@@ -27,3 +27,18 @@ SELECT productName,productScale FROM products;
 
 --List all the territories where we have Offices
 select distinct territory from offices;
+
+--select contact firstname, contact lastname and credit limit for all Customers where credit limit > 30000
+SELECT contactFirstName,contactLastName,creditLimit FROM customers
+where creditLimit > 30000;
+
+--List all Offices not in the USA (4)
+SELECT officecode FROM Offices
+WHERE country!='USA';
+
+--List Orders made between June 16, 2014 and July 7, 2014 (8)
+SELECT ordernumber FROM Orders
+WHERE orderdate BETWEEN '2003-06-01' AND '2004-06-01';
+-- we can write same query using (<,>)
+SELECT ordernumber FROM orders
+WHERE orderdate >= '2003-06-01' AND orderdate <='2004-06-01';
